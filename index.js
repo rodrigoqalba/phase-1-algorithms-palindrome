1afb2558837fe70a6e349fc51264c756d1d2a6af
Rodrigo Alba <rodrigoqalba@gmail.com>
2021-09-27 11:26:43 -0400

    commit tree 5c9180f1e6ea0f3e989e57ce3f067609d3a6c45c

--- index.js
+++ index.js
@@ -1,40 +1,62 @@
-function isPalindrome(word) {
-  // Write your algorithm here
-  
+// Write your algorithm here
+// function isPalindrome(word){
+//   const reversed = word.split('').reverse().join('')
+//   return reversed == word
+// }
 
 function isPalindrome(word){
-  const arrayWord = word.split(``);
-  const reverseArrayWord = arrayWord.reverse();
-  const reverseWord = reverseArrayWord.join(``); 
+for (let i = 0; i < word.length / 2; i++) {
+  const j = word.length - 1 - i;
+  if (word[i] !== word[j]) return false;
+  }
 
-  if(word === reverseWord) {
-    console.log("true")
-    return "true";
-  }
-  else { 
-    console.log("false")
-    return "false";
-  }
+return true;
 }
 
-}
+// function isPalindrome(word){
+//   let start = 0;
+//   let finish = word.length - 1;
 
-const word = isPalindrome("abba")
-console.log(word)
+//   while (start < finish) {
+//     if (word[start++] !== word[finish--]) {
+//        return false
+//     }
+//   }
+//   return true
+// }
+
+
+// function isPalindrome(word){
+//   const arrayWord = word.split(``);
+//   const reverseArrayWord = arrayWord.reverse()
+//   const reverseWord = reverseArrayWord.join(``); 
+
+//   if(word === reverseWord) {
+//     console.log("true");
+
+//   }
+//   else { 
+//     console.log("false");
+
+//   }
+// }
+
+// const word = promt ("abba");
+// isPalindrome(word)
 
 
 
-/* 
-  Add your pseudocode here
-*/
+// /* 
+//   Add your pseudocode here
+// */
 
-/*
-  Add written explanation of your solution here
-*/
+// /*
+//   Add written explanation of your solution here
+// */
 
-// You can run `node index.js` to view these console logs
+// // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
+//   // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
